7b23630dd18fce36e10827da3d65341796239516
Aleksey Monakhov <goshagriboedskii@gmail.com>
2023-02-28 23:29:34 +0200

    upd

--- practice-next/pages/index.js
+++ practice-next/pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +119,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
